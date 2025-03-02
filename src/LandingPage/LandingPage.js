ef1401e8fa523736b984d7fab8a17b6db4a03306
ronit-singh-snow <ronit.singh.snow@gmail.com>
2025-03-02 10:29:17 +0530

    Minor changes

--- src/LandingPage/LandingPage.js
+++ src/LandingPage/LandingPage.js
@@ -19,15 +19,15 @@ function LandingPage() {
     }, []);
 
     return (
-        <div className="main-content">
-            <main>
+        <div >
+            <main className="main-content">
             {show && (
                 <div className='app-banner'>
                     <div className='app-description'>
                         Online ordering is currently available from the app. Click on the below link to download the app.
                     </div>
                     <div className="app-link">
-                        <a href="https://play.google.com/store/apps/details?id=com.vgram.cart&pcampaignid=web_share">
+                        <a href="https://play.google.com/store/apps/details?id=com.vgram.cart&pcampaignid=web_share" target="_blank">
                             <img src="https://cloud.appwrite.io/v1/storage/buckets/6723636300050d7a50f2/files/676d678d002e234b1e18/view?project=66efac960005a73d7247&project=66efac960005a73d7247&mode=admin" className='play-icon' alt='landing page'/>
                             <span className='download-text'>Download Android App</span>
                         </a>
